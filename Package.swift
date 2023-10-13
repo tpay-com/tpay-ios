e383f5697f981be337842c84002dbabe51639582
Tpay Auto Commit <devs@tpay.com>
2023-10-13 12:56:33 +0200

    Main to master change

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
       .binaryTarget(
         name: "Tpay",
-        url: "https://github.com/tpay-com/tpay-ios/releases/download/0.0.4/Tpay.xcframework.zip",
-        checksum: "70d20dded085e0e0fdd2fcbc4190da3ea20792bb4874fdacbb5d6c19e46e8bdb"
+        url: "TO_BE_REPLACED_DURING_CI_BUILD",
+        checksum: "TO_BE_REPLACED_DURING_CI_BUILD"
       )
     ]
 )
